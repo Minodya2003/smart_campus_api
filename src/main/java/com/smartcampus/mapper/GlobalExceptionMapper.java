7a8047bb78359b31666fedfd9478a9f256769bc6
Minodya Jayasingha <minodyaj@gmail.com>
2026-04-24 09:05:00 +0530

    Add global exception mapper to handle unexpected 500 server errors

---
+++ src/main/java/com/smartcampus/mapper/GlobalExceptionMapper.java
@@ -0,0 +1,19 @@
+package com.smartcampus.mapper;
+import com.smartcampus.dto.ErrorResponse;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.*;
+
+@Provider
+public class GlobalExceptionMapper implements ExceptionMapper<Throwable> {
+    @Override
+    public Response toResponse(Throwable t) {
+        if (t instanceof javax.ws.rs.WebApplicationException) {
+            return ((javax.ws.rs.WebApplicationException) t).getResponse();
+        }
+        return Response.status(500)
+                .entity(new ErrorResponse(500, "An unexpected error occurred."))
+                .type(MediaType.APPLICATION_JSON)
+                .build();
+    }
+}
