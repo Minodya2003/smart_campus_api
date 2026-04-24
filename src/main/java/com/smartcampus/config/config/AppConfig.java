dd75c83f073404ecbaa5bdb48302a1bab03c27da
minodya2003 <minodyaj@gmail.com>
2026-04-24 10:25:58 +0530

    Remove duplicated java packages

--- src/main/java/com/smartcampus/config/config/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.smartcampus.config;
-
-import org.glassfish.jersey.server.ResourceConfig;
-
-public class AppConfig extends ResourceConfig {
-    public AppConfig() {
-        packages("com.smartcampus");
-    }
-}
